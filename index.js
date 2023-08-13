ddf00a21673855aebce657a656cdf867227c952d
EzatRashid <ezat.b.rashid@gmail.com>
2023-08-13 13:02:54 -0600

    javascript

--- index.js
+++ index.js
@@ -16,4 +16,56 @@ let events = {
         up:'mouseup'
 
     },
+    touch:{
+        down : 'touchstart',
+        move:'touchmove',
+        up: 'touchend',
+    },
 }
+
+
+let deviceType = '';
+
+let draw = false 
+let erase = false
+
+const isTouchDevice = () =>{
+    try{
+        document.createEvent("TouchEvent")
+        deviceType = 'touch'
+        return true
+    } catch(e){
+        deviceType = 'mouse'
+        return false
+    }
+}
+
+isTouchDevice();
+
+gridButton.addEventListener('click' ,() =>{
+    container.innerHTML = '';
+    let count = 0;
+    for(let i = 0; i < gridHeight.ariaValueMax; i++){
+        count +=2;
+        let div = document.createEvent('div')
+        div.classList.add('gridRow')
+
+        for(let j =0;j < gridWidth.ariaValueMax; j++){
+            count+=2
+            let col = document.createEvent('div')
+            col.classList.add('gridCol')
+            col.setAttribute('id',`gridCol${count}`)
+            col.addEventListener(events[deviceType].down,() =>{
+                draw = true
+                if(erase){
+                    col.style.backgroundColor = 'transparent'
+                }else{
+                    col.style.backgroundColor = colorButton.value
+                }
+            })
+            col.addEventListener(events[deviceType].move,(e) =>{
+                let elementId = document.elementFromPoint
+            })
+        }
+    }
+})
